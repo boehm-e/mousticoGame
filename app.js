81f20b8cccb8db73b18893e07a9a9d26b9d50147
BOEHM Erwan <boehm_e@etna-alternance.net>
2016-12-13 16:13:36 +0100

    fix json pai

--- app.js
+++ app.js
@@ -9,13 +9,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     next();
 });
+app.use(bodyParser.json());
 app.use("/", assignUser);
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    next();
-});
 
 // Routes
 app.post("/api/v1/login", require('./routes/login'));
